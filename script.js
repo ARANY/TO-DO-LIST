b9a034152fbd6d9ca01dcba96835206140a0db9a
ARANY <v.aderibigbe96@gmail.com>
2022-02-22 09:59:04 +0100

    added the clock and to do list

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-function myFunc (){
-alert("You are good")
-};
-
-document.addEventListener("click", myFunc)
-
-
-let clockEvent = document.getElementById("clock");
+let clock = document.getElementById("clock")
 
 function myTime(){
-    const d = new Date();
-    let hourTime = d.getHours();
-    let minuteTime = d.getMinutes();
-    let secondTime = d.getSeconds();
-
-    clockEvent.innerHTML = `${hourTime}:${minuteTime}:${secondTime}!`
-
+  d = new Date();
+  let hoursNow = d.getHours();
+  let minutesNow = d.getMinutes();
+  let secondsNow = d.getSeconds();
+  if (secondsNow <10){
+    secondsNow = "0"+secondsNow;
+  }
+  if (minutesNow <10){
+    minutesNow = "0"+minutesNow};
+  clock.innerHTML = `${hoursNow}:${minutesNow}:${secondsNow}`
 }
 
 setInterval(myTime,1000);
 
+let ourForm = document.getElementById("ourForm");
+let ourInput = document.getElementById("ourInput");
+let ourList = document.getElementById("ourList");
+
+ourForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createItem(ourInput.value);
+})
+ 
+ function createItem(x){
+   ourHTML = `<li>${x}  <button onclick = "deleteItem(this)">delete</button></li>`
+   ourList.insertAdjacentHTML("beforeend",ourHTML)
+   ourInput.value = "";
+   ourInput.focus();
+   
+ }
+
+function deleteItem(elementToDelete){
+  elementToDelete.parentElement.remove()
+}
