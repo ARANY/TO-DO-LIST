832ecd34700b816e87caa1dd419ce0b12c8d78a7
ARANY <v.aderibigbe96@gmail.com>
2022-02-25 13:56:44 -0500

    added css & js

--- tipcalculate.js
+++ tipcalculate.js
@@ -1,3 +1,8 @@
+
+
+function chek(x){
+    alert(`the ${x}`)
+}
 function calculateTip(){
    var billAMount = document.getElementById("billamount").value;
    var serviceqty = document.getElementById("serviceqty").value;
@@ -6,30 +11,39 @@ function calculateTip(){
    if(billAMount === "" || serviceqty == 0){
        alert("You have to enter value")
    }
-   if(people == "" || people <= 1){
-       people = 1;
-       document.getElementById("each").style.display = none;
+   else if(people === "" || people <= 1){
+       people = 1
+       document.getElementById("each").style.display = "none";
    } else {
-       document.getElementById("each").style.display = block;
-   }
-
+       document.getElementById("each").style.display = "block";
+    }
    //calculate the tip
 
    var totalTip = (billAMount * serviceqty) / people;
-   var totalTip = Math.round(totalTip /100) * 100;
 
-   totalTip = totalTip.toFixed(2);
+   //var totalTip = Math.round(totalTip);
+   var totalTip = totalTip.toFixed(2);
+   //var totalTip = Math.round(totalTip);
+
+   //var totalTip = totalTip.toFixed(2);
+  
+
+
    //display the totalTip
-   document.getElementById("totalTip").style.display = block;
-   document.getElementById("tip").style.display = totalTip;
+   document.getElementById("totalTip").style.display = "block";
+   document.getElementById("tip").innerHTML = totalTip;
 }
-
 //prevent tipamount on reload
+newFunction();
 
-document.getElementById("totalTip").style.display = none;
-document.getElementById("each").style.display = none;
+
+function newFunction() {
+    document.getElementById("totalTip").style.display = "none";
+    document.getElementById("each").style.display = "none";
+}
 
 // click to call function 
-document.getElementById("calculateButton").onclick = function (){
+document.getElementById("calculateButton").onclick = () => {
     calculateTip();
-}
+};
+
